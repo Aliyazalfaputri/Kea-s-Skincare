26be10a470a1fa9b095bde847e1461ed52ed74ae
Aliyazalfaputri <aliyazalfa186@gmail.com>
2024-06-14 18:16:35 +0700

    revisi terakhir banget

--- create_admin.php
+++ create_admin.php
@@ -1,20 +1,34 @@
 <?php
 include 'config.php';
 
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
 // Username dan password admin
 $username = 'adminkea';
 $password = password_hash('123', PASSWORD_DEFAULT);
 
-// Query untuk memasukkan data admin ke tabel users
-$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+// Cek apakah username sudah ada
+$sql_check = "SELECT * FROM users WHERE username = '$username'";
+$result_check = $conn->query($sql_check);
 
-// Eksekusi query
-if ($conn->query($sql) === TRUE) {
-    echo "Admin berhasil ditambahkan";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if ($result_check->num_rows == 0) {
+    // Query untuk memasukkan data admin ke tabel users
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    // Eksekusi query
+    $conn->query($sql);
 }
 
 // Tutup koneksi
 $conn->close();
+
+// Redirect to index.php
+header("Location: index.php");
+exit();
 ?>
